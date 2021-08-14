789f769caa4a5e93c8914fb3f93438237fd58d69
Ravie403 <info@tkm.blue>
2021-08-15 08:08:37 +0900

    change constructor

--- src/scripts/random.ts
+++ src/scripts/random.ts
@@ -3,7 +3,7 @@ export const Random = class {
   private x: number;
   private y: number;
   private z: number;
-  constructor(seed: number) {
+  constructor(seed?: number) {
     this.w = seed ?? Math.floor(Math.random() * 1e9);
     this.x = 483210922;
     this.y = 854321471;
@@ -12,10 +12,12 @@ export const Random = class {
   public int() {
     // xor shift
     const t = this.x ^ (this.x << 11);
-    this.x = this.y;
-    this.y = this.z;
-    this.z = this.w;
-    this.w = this.w ^ (this.w >>> 19) ^ (t ^ (t >>> 8));
+    [this.x, this.y, this.z, this.w] = [
+      this.y,
+      this.z,
+      this.w,
+      this.w ^ (this.w >>> 19) ^ (t ^ (t >>> 8)),
+    ];
     return Math.abs(this.w);
   }
   public rangeInt(min = 0, max = 1e9) {
