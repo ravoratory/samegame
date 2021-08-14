97922c6e07df590286641fc51632d9dde7cda73d
Ravie403 <info@tkm.blue>
2021-08-15 08:04:10 +0900

    variables to private

--- src/scripts/random.ts
+++ src/scripts/random.ts
@@ -1,15 +1,15 @@
 export const Random = class {
-  w: number;
-  x: number;
-  y: number;
-  z: number;
+  private w: number;
+  private x: number;
+  private y: number;
+  private z: number;
   constructor(seed: number) {
     this.w = seed ?? Math.floor(Math.random() * 1e9);
     this.x = 483210922;
     this.y = 854321471;
     this.z = 280934432;
   }
-  int() {
+  public int() {
     // xor shift
     const t = this.x ^ (this.x << 11);
     this.x = this.y;
@@ -18,7 +18,7 @@ export const Random = class {
     this.w = this.w ^ (this.w >>> 19) ^ (t ^ (t >>> 8));
     return Math.abs(this.w);
   }
-  rangeInt(min = 0, max = 1e9) {
+  public rangeInt(min = 0, max = 1e9) {
     const r = this.int();
     return min + (r % (max + 1 - min));
   }
